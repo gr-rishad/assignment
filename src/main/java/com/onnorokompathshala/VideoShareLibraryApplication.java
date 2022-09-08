835d2b76b19556bef657b98791111e7708ffb1f5
Golam Rabbani Rishad <33056647+gr-rishad@users.noreply.github.com>
2022-09-08 19:41:49 +0600

    project initialize

---
+++ src/main/java/com/onnorokompathshala/VideoShareLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.onnorokompathshala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoShareLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoShareLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onnorokompathshala/VideoShareLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onnorokompathshala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoShareLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
